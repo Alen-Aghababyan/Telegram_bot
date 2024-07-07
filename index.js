e14b389cf0ab64b47eb18cc35b1911d59299fd3d
Alen-Aghababyan <alen.aghababyan@tumo.org>
2024-07-07 15:24:39 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api')
+const express = require('express')
 require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 const token = process.env.TOKEN
 
